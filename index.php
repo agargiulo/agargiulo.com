1485d57afb6b296ab1e7ce4884b7fac4d41e817f
Anthony Gargiulo <anthony@agargiulo.com>
2013-10-02 00:44:16 +0000

    updates to fix the RIT year

--- index.php
+++ index.php
@@ -1,75 +1,80 @@
 <!DOCTYPE HTML>
 <html>
   <head>
-    <meta charset="utf-8">
-    <title>Anthony Gargiulo</title>
-    <link rel="stylesheet" href="stylesheets/styles.css">
-    <link rel="stylesheet" href="stylesheets/pygment_trac.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-    <script src="javascripts/respond.js"></script>
-    <script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
-    <!--[if lt IE 9]>
-    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-    <!--[if lt IE 8]>
-    <link rel="stylesheet" href="stylesheets/ie.css">
-    <![endif]-->
-    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+	<meta charset="utf-8">
+	<title>Anthony Gargiulo</title>
+	<link rel="stylesheet" href="stylesheets/styles.css">
+	<link rel="stylesheet" href="stylesheets/pygment_trac.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script src="javascripts/respond.js"></script>
+<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
+<!--[if lt IE 9]>
+<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<!--[if lt IE 8]>
+<link rel="stylesheet" href="stylesheets/ie.css">
+<![endif]-->
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 
-  </head>
-  <body>
-<?php include_once("analyticstracking.php");
-      include_once("gravatar.php");?>
-    <a href="https://github.com/agargiulo"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub"></a>
-    <div class="wrapper">
+</head>
+<body>
+<?
+if ($_SERVER['SERVER_NAME'] == "www.agargiulo.com")
+{
+	include_once("analyticstracking.php");
+}
+include_once("gravatar.php");
+include_once("environment.php");?>
+<a href="https://github.com/agargiulo"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub"></a>
+	<div class="wrapper">
 
-      <section>
-      <div id="title">
-        <h1>Anthony Gargiulo</h1>
-        <hr>
-        <span class="credits left">Project maintained by <a href="https://github.com/agargiulo">agargiulo</a></span>
-        <span class="credits right">Theme by <a href="http://twitter.com/#!/michigangraham">mattgraham</a></span>
-      </div>
+	<section>
+	<div id="title">
+	<h1>Anthony Gargiulo</h1>
+	<hr>
+	<span class="credits left">Project maintained by <a href="https://github.com/agargiulo">agargiulo</a></span>
+	<span class="credits right">Theme by <a href="http://twitter.com/#!/michigangraham">mattgraham</a></span>
+	</div>
 
-	  <p style="float:right"><img src="<?=$grav_url?>" alt="Gravatar Avatar"></p>
-      <h1>About Me</h1>
+	<p style="float:right"><img src="<?=$grav_url?>" alt="Gravatar Avatar"></p>
+	<h1>About Me</h1>
 
-      <p>I'm a third year student currently studying Computer Science at Rochester Institute of Technology.
-      I am an RTP (Root-Type Person) for Computer Science House (<a href="http://www.csh.rit.edu">CSH</a>).<br>
-      For fun, I spend a large portion of my time tinkering with either my Linux boxen or random code projects that I find interesting. <br></p>
+	<p>I'm a <?=$RITYearLevel?> year student currently studying Computer Science at Rochester Institute of Technology.
+	I am an RTP (Root-Type Person) for Computer Science House (<a href="http://www.csh.rit.edu">CSH</a>).<br>
+	For fun, I spend a large portion of my time tinkering with either my Linux boxen or random code projects that I find interesting. <br></p>
 
 
-      <h2>Resume</h2>
+		<h2>Resume</h2>
 
-      <p><a href="http://resume.agargiulo.com/resume.pdf">PDF Copy </a>
-      <br><a href="http://resume.agargiulo.com/resume.tex.html">LaTeX source</a>
-      <br><a href="https://github.com/agargiulo/resume">Github repo</a>
-      <br></p>
+		<p><a href="http://resume.agargiulo.com/resume.pdf">PDF Copy </a>
+		<br><a href="http://resume.agargiulo.com/resume.tex.html">LaTeX source</a>
+		<br><a href="https://github.com/agargiulo/resume">Github repo</a>
+		<br></p>
 
-      <hr>
-      <h2>Contact Info</h2>
+		<hr>
+		<h2>Contact Info</h2>
 
-      <p><a href="mailto:anthony@agargiulo.com">anthony@agargiulo.com</a><br>
-	  <a href="mailto:agargiulo@csh.rit.edu">agargiulo@csh.rit.edu</a><br>
-	  <a href="mailto:amg6763@rit.edu">amg6763@rit.edu</a><br>
-	  </p>
-	  <hr>
-	  <a href="https://projecteuler.net/progress=agargiulo">
-	  <img src="https://projecteuler.net/profile/agargiulo.png" alt="ProjectEuler profile for agargiulo"/>
-	  </a>
-	  <br>
-	  <a href="https://ipv6.he.net/certification/scoresheet.php?pass_name=agargiulo" target="_blank"><img src="https://ipv6.he.net/certification/create_badge.php?pass_name=agargiulo&amp;badge=3" style="border: 0; width: 229px; height: 137px" alt="IPv6 Certification Badge for agargiulo"></a>
-	  <br/>
-	  <a href="https://twitter.com/argargablar" class="twitter-follow-button" data-show-count="false">Follow @argargablar</a>
-	  <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-	  <br/>
-	  <div class="g-plusone" data-href="https://www.agargiulo.com"></div>
-	  <a href="https://www.linkedin.com/pub/anthony-gargiulo/44/526/b1a">
-	  <img src="https://www.linkedin.com/img/webpromo/btn_viewmy_120x33.png" style="width: 120; height: 33; border: 0;" alt="View Anthony Gargiulo's profile on LinkedIn">
-	  </a>
-	  </section>
-    </div>
-    <!--[if !IE]><script>fixScale(document);</script><![endif]-->
+		<p><a href="mailto:anthony@agargiulo.com">anthony@agargiulo.com</a><br>
+		<a href="mailto:agargiulo@csh.rit.edu">agargiulo@csh.rit.edu</a><br>
+		<a href="mailto:amg6763@rit.edu">amg6763@rit.edu</a><br>
+		</p>
+		<hr>
+		<a href="https://projecteuler.net/progress=agargiulo">
+		<img src="https://projecteuler.net/profile/agargiulo.png" alt="ProjectEuler profile for agargiulo"/>
+		</a>
+		<br>
+		<a href="https://ipv6.he.net/certification/scoresheet.php?pass_name=agargiulo" target="_blank"><img src="https://ipv6.he.net/certification/create_badge.php?pass_name=agargiulo&amp;badge=3" style="border: 0; width: 229px; height: 137px" alt="IPv6 Certification Badge for agargiulo"></a>
+		<br/>
+		<a href="https://twitter.com/argargablar" class="twitter-follow-button" data-show-count="false">Follow @argargablar</a>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+<br/>
+	<div class="g-plusone" data-href="https://www.agargiulo.com"></div>
+	<a href="https://www.linkedin.com/pub/anthony-gargiulo/44/526/b1a">
+	<img src="https://www.linkedin.com/img/webpromo/btn_viewmy_120x33.png" style="width: 120; height: 33; border: 0;" alt="View Anthony Gargiulo's profile on LinkedIn">
+	</a>
+	</section>
+	</div>
+	<!--[if !IE]><script>fixScale(document);</script><![endif]-->
 
-  </body>
-</html>
+	</body>
+	</html>
