bf43b8646f8373ea51a7e8d04669029ea58043c1
Anthony Gargiulo <anthony@agargiulo.com>
2016-08-23 20:07:04 -0400

    Case changes are important too

--- environment.php
+++ environment.php
@@ -1,4 +1,4 @@
 <?php
 $meta_page_title = "Anthony Gargiulo's personal site";
-$meta_page_desc = "home page for Anthony Gargiulo <anthony@agargiulo.com>";
+$meta_page_desc = "Home page for Anthony Gargiulo <anthony@agargiulo.com>";
 ?>
